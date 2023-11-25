36a92d3861bd501735913cd4012a04983cba4d75
zin <rangararazin@outlook.com>
2023-11-25 00:37:00 +0000

    refactor one router to one controller

--- Core/Database.php
+++ Core/Database.php
@@ -1,46 +1,50 @@
 <?php
 
-    class Database
+namespace Core;
+
+use PDO;
+
+class Database
+{
+    public $connection;
+    public $statement;
+
+    public function __construct($config, $username = 'root', $password = 'test')
     {
-        public $connection;
-        public $statement;
+        $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-        public function __construct($config, $username = 'root', $password = 'Test@101')
-        {
-            $dsn = 'mysql:' . http_build_query($config, '', ';');
-
-            $this->connection = new PDO($dsn, $username, $password, [
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-            ]);
-        }
-
-        public function query($query, $params = [])
-        {
-            $this->statement = $this->connection->prepare($query);
-
-            $this->statement->execute($params);
-
-            return $this;
-        }
-
-        public function get()
-        {
-            return $this->statement->fetchAll();
-        }
-
-        public function findOrFail()
-        {
-            $result = $this->find();
-
-            if (!$result) {
-                abort();
-            }
-
-            return $result;
-        }
-
-        public function find()
-        {
-            return $this->statement->fetch();
-        }
+        $this->connection = new PDO($dsn, $username, $password, [
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
     }
+
+    public function query($query, $params = [])
+    {
+        $this->statement = $this->connection->prepare($query);
+
+        $this->statement->execute($params);
+
+        return $this;
+    }
+
+    public function get()
+    {
+        return $this->statement->fetchAll();
+    }
+
+    public function find()
+    {
+        return $this->statement->fetch();
+    }
+
+    public function findOrFail()
+    {
+        $result = $this->find();
+
+        if (! $result) {
+            abort();
+        }
+
+        return $result;
+    }
+}
